7d3867c3e7815f85b1be40bb0a6e6e8df50e8760
Walker Le <walker.le88@gmail.com>
2022-12-20 10:58:41 -0700

    commit tree e84a47e058525d881594836b441806e0dedcdcc7

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// console.log(saturdayFun());
+// console.log(saturdayFun('sleep'));
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+// console.log(mondayWork());
+// console.log(mondayWork('go to class'));
+
+function wrapAdjective(flair = '*') {
+    function newFunc(para1 = 'special') {
+        return `You are ${flair}${para1}${flair}!`;
+    }
+    return newFunc;
+}
